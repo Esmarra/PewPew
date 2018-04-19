809383dd481c0166623f0b1e8bc12dea0a687e5c
esmarra <miguel.maranha@gmail.com>
2018-04-19 19:11:38 +0100

    bugged flt_max

--- Projecto/3Ex/Client/NOVO/REBOOT/client.c
+++ Projecto/3Ex/Client/NOVO/REBOOT/client.c
@@ -31,9 +31,10 @@ Special Thanks:
 //====  MMAP ====//
 #include <sys/mman.h> // MAP_PRIVATE
 #include <sys/types.h>
+#include <stdint.h> //unit64 <-- remove?
+//==== FILE I/O ====//
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <stdint.h> //unit64 <-- remove?
 
 #define MAX_CAR 30 // File Max Characters per Line
 #define FLT_MAX 20 // Max lines(aka numbers) is .ASC FILE
@@ -45,18 +46,11 @@ void fp2bin_f(double fp_frac, char* binString);
 void fp2bin(double fp, char* binString);
 char bin[MANTISSA];
 
-struct Data{
-  float number; // Stores in Float Array
-  //float teste;
-  int data_size; // Keeps Track of Array Size
-};
-
 float dbin[FLT_MAX];
 
 int main(int argc, char **argv){
   int sock;
   struct sockaddr_un server;
-  struct Data data[FLT_MAX]; //Start data[INDEX] struct #### DELETE ####
   int line=1; //Number of Lines in file AKA FLT_MAX
 
   FILE *ficheiro1;
@@ -79,50 +73,51 @@ int main(int argc, char **argv){
   printf(">Input File has %d lines\n",line );
 
   //==== Open .bin ====//   ########## BUGGADO ##########
-  char *memblock;
+  char buffer[100];
+  int flt=0; // Number of Floats/lines in .bin
+  /*
+  FILE *fp;
+  fp = fopen("input.bin", "rb");
+  fread(buffer, sizeof(double), 1, fp);
+  printf("wtf:%s endl\n", buffer);
+  fclose(fp);
+  */
+  const char *memblock;
   int fd;
   size_t size;
   fd = open("input.bin", O_RDONLY); //input.bin
   //printf("Size: %zd\n",sb.st_size);
   size = lseek(fd, 0, SEEK_END); // Find Max Size
   lseek(fd, 0, SEEK_SET); // Reset cursor 0
-  printf("Size2: %zd\n",size);
+  printf("File Size: %zd bytes\n",size);
   memblock = mmap(NULL, size,PROT_READ, MAP_PRIVATE, fd, 0);
   if (memblock == MAP_FAILED){
     perror("\nError|mmap");
     exit(EXIT_FAILURE);
   }
-
-  char teste[100];
-  char aux[1];
-  char outra[30];
-  float value[100];
+  char aux[2];
+  int line_size[5]; // Cheat (we know how many lines file has)
   int i;
-  for(i = 0; i < size; i++){ // Abrir x mem blocks (MAX?)
+  for(i = 0; i < size+1; i++){ // Abrir <FILE SIZE> mem blocks
     if(memblock[i]=='\n'){ //end strcat
-      //value=atof(teste);
-      sprintf(outra, "%.20f", atof(teste));
-      printf("Bin Flt: %s \n",outra);
-      teste[0]='\0'; //Reset temp
-      //printf("endl\n" );
-      //flt++;
+      dbin[flt]=atof(buffer); // Conv float e store num flt array
+      printf("\nBin %d: %f \n",flt,dbin[flt]);
+      printf("Bin Size: %d\n",line_size[flt]);
+      buffer[0]='\0'; //Reset temp
+      flt++; // Increment flt (=FLT_MAX)
     }
     else{
       if(memblock[i]=='\0'){
-        printf("dead\n" );
+        printf("End file\n" ); // <-- We never get here (use size+1) to get here
         break;
       }
-      sprintf(aux,"%c",memblock[i]); // copy memblock to aux
-      //printf("aux:%s\n",aux);
-      strcat(teste,aux); // clip aux to temp
+      sprintf(aux,"%c",memblock[i]); // copy data in memblock to aux
+      strcat(buffer,aux); // clip aux to temp
+      //printf("aux:%s\n",buffer);
+      line_size[flt]++; // Incrment size of each line
     }
    }
-
-
-  float nuk;
-  nuk=read(fd, memblock, size);
-  printf("Nuk: %f\n",nuk);
-  close(fd);
+   close(fd);
   //==== XXXXXXXX ====// ########## BUGGADO ##########
 
 
@@ -139,12 +134,12 @@ int main(int argc, char **argv){
       exit(0);
   }
 
-  float info;
+  float info; // podemos eleminar
   do{
     int i;
-    for(i=0;i<data[line].data_size;i++){
-      info=data[line].number;
-      printf("Sent%f\n",info); // DEBUG
+    for(i=0;i<flt;i++){
+      info=dbin[i];
+      printf("Sent %f\n",info); // DEBUG
       if(send(sock,&info,sizeof(info),0)<0){// Send Data to Server
         perror("Sending Data");
         exit(0);
