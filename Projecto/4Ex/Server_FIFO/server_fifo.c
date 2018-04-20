dccc1996c6c4547174da14ac3701d40908272c84
esmarra <miguel.maranha@gmail.com>
2018-04-20 18:11:35 +0100

    Server 99.9%

--- Projecto/4Ex/Server_FIFO/server_fifo.c
+++ Projecto/4Ex/Server_FIFO/server_fifo.c
@@ -4,7 +4,7 @@ Base Language:	Mixed
 Created by   :	Esmarra
 Creation Date:	19/04/2018
 Rework date entries:
-
+  *20/04/2018
 Program Objectives:
 Observations:
 Special Thanks:
@@ -21,34 +21,69 @@ Special Thanks:
 #include <stdint.h> //unit64 <-- remove?
 #include <sys/types.h>
 #define MANTISSA 50 // Bits Res in conv to bin
+#define FLT_MAX 20 // Max lines(aka numbers) is .ASC FILE
 
 char bin[MANTISSA];
+float dflt[FLT_MAX]; // Array que da store aos floats
+int max;
+
 float bin2fp(char* digits);
 
 int main(int argc, char **argv){
-  int fd;
-  char buffer[50];
+  int fifo_fd; // FIFO file descriptor
+  char buffer[50]; // FIFO buffer
   float buf;
+  int count;
+  count=0;
+  //int file_fd = open("output.txt",O_WRONLY|O_CREAT|O_TRUNC,0666);
   FILE *ficheiro1;
-  ficheiro1 = fopen("output.asc","w");
-  fd=open("/tmp/myfifo",O_RDONLY);
-
-  while (1){
-    if (read(fd,buffer,sizeof(buffer)) > 0){
-      printf("%s \n", buffer);
-      buf=bin2fp(buffer); // Conv 2 flt
-      fprintf(ficheiro1, "%f\n",buf);
-      printf("Coverted:%f\n",buf);
-      exit(0);
-    }
+  if((ficheiro1 = fopen("output.asc","w")) == NULL){
+    perror("\nError|Open output.asc");
+    exit(EXIT_FAILURE);
   }
 
-  //fflush(stdin);
-  //write(1,buffer,sizeof(buffer));
-  //printf("\n");
+  if((fifo_fd=open("/tmp/myfifo",O_RDONLY)) == -1){
+		perror("\nError|Open FIFO");
+    exit(EXIT_FAILURE);
+	}
 
-  close(fd);
-  return(0);
+  while(1){
+    if(read(fifo_fd,buffer,sizeof(buffer)) > 0){
+      if(strcmp(buffer,"flt_max")==0){
+        read(fifo_fd,buffer,sizeof(buffer));// read 1 is null :/
+        read(fifo_fd,buffer,sizeof(buffer));
+        printf("Rcv_flt_max= %s\n",buffer );
+        max=atoi(buffer);
+        printf("FLT_MAX= %d\n",max);
+        break;
+      }
+      if(strcmp(buffer,"\0")!=0){ // If its not a \0
+        printf("Rcv: %s\n",buffer );
+        buf=bin2fp(buffer); // Conv 2 flt
+        printf("Coverted:%f\n",buf);
+        dflt[count]=buf;
+        count++;
+      }
+        //bzero(buffer,256);
+        //==== PRINTF ====//
+        //fprintf(ficheiro1,"%f\n",buf); //write to file
+        //==== WRITE ====//
+        //write(file_fd,buffer,sizeof buffer);
+        //==== FWRITE ====//
+        //fwrite(buffer,1,6,rtard);
+        //close(file_fd);
+      //break;
+    }
+  }
+  int i;
+  for(i=0;i<max;i++){
+    fprintf(ficheiro1,"%f\n",dflt[i]); //write to file
+  }
+
+  //close(file_fd);
+  fclose(ficheiro1);
+  close(fifo_fd);
+  exit(0);
 }
 
 float bin2fp(char* digits){
